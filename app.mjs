860164db8bb512c266e15dc2c32319438896cda8
Park Joyeong <drhamst@gmail.com>
2025-12-09 16:24:28 +0900

    회원가입 준비

---
+++ app.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import authRouter from "./router/auth.mjs";
+import { config } from "./config.mjs";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(config.host.port);
